45a17f44c8158b78271527c6b2a4937628441a76
Roman Palna <roma.palna@gmail.com>
2020-10-09 15:01:05 +0300

    start

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const exhbs = require('express-handlebars');
+const app = express();
+
+const wine = require('./wine.json')
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exhbs({
+    extname: 'hbs',
+  }),
+);
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
+});
+
+app.get('/wine', (req, res) => {
+    res.render('wine', {wine, cssFileName: 'wine'})
+})
+
+app.listen(4444, () => {
+    console.log(`Application server is running on port ${4444}`);
+});
